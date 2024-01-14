d0911cd175970e50ca2807aa54a1936588229e5d
Jorge Ariza <v.ariza.0511@gmail.com>
2024-01-14 17:16:58 -0600

    Primer funcion con metodo GET

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI #importar clase FastAPI
 
-app = FastAPI(title = 'Proyecto para reseniar peliculas',
-              description = 'En este proyecto, seremos capaces de reseniar peliculas',
-              version = '1'
+app = FastAPI(title = 'Proyecto para reseniar peliculas', #titulo de proyecto
+              description = 'En este proyecto, seremos capaces de reseniar peliculas', #description for project
+              version = '1' #versionar proyecto
               )
+
+#La funcion encargada de retornar respuesta
+#Si hay multiples funciones, se completen de forma asincrona
+@app.get('/')
+async def index():
+    return 'Hola mundo, desde un server en FastAPI'
