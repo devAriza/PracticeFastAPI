fe6bdad04e7ca9ea914abd9f9720e9bc846f4ff7
Jorge Ariza <v.ariza.0511@gmail.com>
2024-01-14 16:57:05 -0600

    Crear proyecto, con primera instancia de FastAPI

---
+++ main.py
@@ -0,0 +1,6 @@
+from fastapi import FastAPI
+
+app = FastAPI(title = 'Proyecto para reseniar peliculas',
+              description = 'En este proyecto, seremos capaces de reseniar peliculas',
+              version = '1'
+              )
