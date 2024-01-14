766840c324844bd16f574bd20ea85ef0de300763
Jorge Ariza <v.ariza.0511@gmail.com>
2024-01-14 17:26:51 -0600

    Agregar segundo metodo GET, con la descripcion de la app

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from fastapi import FastAPI #importar clase FastAPI
 
+#Ingresar a documentacion del servicio web con URL/docs
+
+#Crear instancia de FastAPI
 app = FastAPI(title = 'Proyecto para reseniar peliculas', #titulo de proyecto
               description = 'En este proyecto, seremos capaces de reseniar peliculas', #description for project
               version = '1' #versionar proyecto
               )
 
 #La funcion encargada de retornar respuesta
-#Si hay multiples funciones, se completen de forma asincrona
+#Si hay multiples funciones, se completen de forma asincrona con 'async'
 @app.get('/')
 async def index():
     return 'Hola mundo, desde un server en FastAPI'
+
+
+@app.get('/about')
+async def about():
+    return app.description
