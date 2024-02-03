3dfc6174a52d99e8b26741dd5bb930fd795fcbd8
Jorge Ariza <v.ariza.0511@gmail.com>
2024-02-02 23:01:22 -0600

    Agregando variables para asignacion a clase MySQLDatabase

--- database.py
+++ database.py
@@ -1,9 +1,15 @@
-from peewee import *
+from peewee import * 
 
+#Clase MySQLDatabase
 #nameBD
 #user = 'user'
 #password = ''
 #host = 'localhost'
 #port = 3306 #Puerto por default utilizado por MySQL
 
-database = MySQLDatabase('fastapi_project', user = 'root', password = '', host = 'localhost', port = 3306)
+nameBD = 'fastapi_project'
+usuario = 'root'
+hosst = 'localhost'
+puerto = 3306
+
+database = MySQLDatabase(nameBD, user = usuario, password = '', host = hosst, port = puerto)
