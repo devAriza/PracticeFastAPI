3d093c34afe47a3f10b4373659cd8a32d4767269
Jorge Ariza <v.ariza.0511@gmail.com>
2024-02-25 18:07:24 -0600

    Crear endpoint en user para loggeo. Validar datos de entrada con HTTPBasicCredentials

--- project/routers/users.py
+++ project/routers/users.py
@@ -1,4 +1,5 @@
 from fastapi import HTTPException, APIRouter
+from fastapi.security import HTTPBasicCredentials
 from ..database import User
 from ..schemas import UserRequestModel, UserResponseModel
 
@@ -27,4 +28,20 @@ async def create_user(user: UserRequestModel): #indicamos clase de tipo BaseMode
 
 
     #retornar Modelo, que nos permite validar datos de entrada y de salida
+    return user
+
+#Endpoint para login
+#Conocer que usuario se esta autenticando, usuario debe enviar user y password con HTTPUserCredentials
+#25/02/2024
+@router.post('/login', response_model = UserResponseModel)
+async def login(credentials : HTTPBasicCredentials):
+    
+    user = User.select().where(User.username == credentials.username).first()
+
+    if user is None:
+        raise HTTPException(404, 'User not found')
+    
+    if user.password != User.create_password(credentials.password):
+        raise HTTPException(404, 'Password error')
+    
     return user
