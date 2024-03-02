995f7d18234a90fa70330ea79ae6d46d8b6673d9
Jorge Ariza <v.ariza.0511@gmail.com>
2024-03-02 15:07:36 -0600

    Agregar endpoint para obtener las movies creadas

--- project/routers/movies.py
+++ project/routers/movies.py
@@ -1,10 +1,11 @@
 from fastapi import HTTPException, APIRouter
 from ..database import Movie
 from ..schemas import MovieRequestModel, MovieResponseModel
+from typing import List
 
 router = APIRouter(prefix = '/movies')
 
-@router.post('/movies', response_model = MovieResponseModel)
+@router.post('', response_model = MovieResponseModel)
 async def create_movies(movie : MovieRequestModel):
 
     if Movie.select().where(Movie.title == movie.title).exists():
@@ -16,3 +17,10 @@ async def create_movies(movie : MovieRequestModel):
     )
 
     return movie
+
+@router.get('', response_model = List[MovieResponseModel])
+async def get_movies():
+
+    moviess = Movie.select()
+
+    return [movies for movies in moviess]
