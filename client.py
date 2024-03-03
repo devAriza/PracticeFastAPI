5516bc4092ce3a9245b320920273d871c33586bb
Jorge Ariza <v.ariza.0511@gmail.com>
2024-03-03 00:29:37 -0600

    Comentar requests.post y crear requests put & delete

--- client.py
+++ client.py
@@ -44,23 +44,54 @@ import requests
 #             #Accede a la llave score y a la llave reviews del objeto JSON
 #             print(f"score: {review['score']} - {review['reviews']}")
 
-URL = 'http://127.0.0.1:8000/api/v1/reviews'
-#Body para crear una resenia
-REVIEW = {
-    'user_id' : 11,
-    'movie_id' : 1,
-    'reviews' : 'Review creada con requests',
-    'score' : 5
-}
+# ----------------------- requests POST ----------------------- #
+# URL = 'http://127.0.0.1:8000/api/v1/reviews'
+# #Body para crear una resenia
+# REVIEW = {
+#     'user_id' : 11,
+#     'movie_id' : 1,
+#     'reviews' : 'Review creada con requests',
+#     'score' : 5
+# }
 
-response = requests.post(URL, json = REVIEW)
+# response = requests.post(URL, json = REVIEW)
 
+# if response.status_code == 200:
+#     print('Resenia creada de forma exitosa')
+
+#     print(response.json()['id'])
+# else:
+#     print(
+#         #Acceso del contenido de la respuesta
+#         response.content
+#     )
+
+# ----------------------- requests PUT ----------------------- #
+# #Numero de id \ user_review
+# REVIEW_ID = 6
+# URL = f'http://127.0.0.1:8000/api/v1/reviews/{REVIEW_ID}'
+
+# REVIEW = {
+#     'reviews' : 'Review modificada desde requests',
+#     'score' : 1
+# }
+
+# response = requests.put(URL, json=REVIEW)
+# if response.status_code == 200:
+#     print("La resenia se actualizo correctamente")
+#     #Objeto JSON que servidor retorna
+#     print(response.json())
+
+
+# ----------------------- requests DELETE ----------------------- #
+#Numero de id \ user_review
+REVIEW_ID = 4
+URL = f'http://127.0.0.1:8000/api/v1/reviews/{REVIEW_ID}'
+
+response = requests.delete(URL)
 if response.status_code == 200:
-    print('Resenia creada de forma exitosa')
-
-    print(response.json()['id'])
+    print("La resenia fue eliminada de forma correcta")
+    #Objeto JSON que servidor retorna
+    print(response.json())
 else:
-    print(
-        #Acceso del contenido de la respuesta
-        response.content
-    )
+    print(response.content)
